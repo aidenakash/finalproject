fa089cc22e860fd43c7c6a5e01f6e0d5ce898119
aidenakash <96476154+aidenakash@users.noreply.github.com>
2022-04-27 14:25:37 -0500

    final

---
+++ Book-Store-With-MERN-Stack-Backend/app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const router = require("./routes/book-routes");
+const cors = require("cors");
+const app = express();
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
+app.use("/books", router); // localhost:5000/books
+
+mongoose
+.connect("mongodb+srv://Akash:05121998@cluster0.b9jwp.mongodb.net/bookstore?retryWrites=true&w=majority")
+  .then(() => console.log("Connected To Database"))
+  .then(() => {
+    app.listen(5000);
+  })
+  .catch((err) => console.log(err));
